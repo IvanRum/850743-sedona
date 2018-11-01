f3b9896ef24ca25440fda8b0e8b126dc6163ad44
IvanRum <ivanredhill@gmail.com>
2018-11-01 16:43:37 +0400

    Доработка базовых критериев

--- gulpfile.js
+++ gulpfile.js
@@ -71,6 +71,7 @@ gulp.task("html", function() {
 gulp.task("copy", function() {
   return gulp.src([
       "source/fonts/**/*.{woff,woff2}",
+      "source/img/**",
       "source/js/**"
     ], {
       base: "source"
